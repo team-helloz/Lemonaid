af5437af445859d22a2f0eee2226b36b551e6660
ek_13 <kldoggod@gmail.com>
2022-03-20 17:01:11 +0900

    design : 증상/질병 조회 부위 프레임 추가

--- frontend/src/routes/DiseaseInfo.tsx
+++ frontend/src/routes/DiseaseInfo.tsx
@@ -1,10 +1,15 @@
 import NavBar from "../components/NavBar";
+import BodyPart1 from "../components/DiseaseInfo/BodyPart1";
+import BodyPart2 from "../components/DiseaseInfo/BodyPart2";
 
 export default function DiseaseInfo() {
   return (
     <div>
       <NavBar />
       <h1>증상/질병 조회</h1>
+      <BodyPart1 />
+      <br />
+      <BodyPart2 />
     </div>
   );
 }
