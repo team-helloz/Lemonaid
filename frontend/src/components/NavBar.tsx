ce984bfce08c5df6a65904ab88e4a426cce471d2
sanghyun-lee2 <h983019@naver.com>
2022-03-20 13:36:21 +0900

    design : 상단 메뉴바 디자인 적용
    S06P22D108-79
    
    상단 메뉴바 디자인 적용

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,23 +1,67 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import * as S from "./styles";
 
-export default function NavBar() {
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav>
-      <Link to="/">홈화면</Link>
-      <ul>
-        <li>
-          <Link to="/hospital">의료기관 조회</Link>
-        </li>
-        <li>
-          <Link to="/drug">의약품 조회</Link>
-        </li>
-        <li>
-          <Link to="/disease">증상/질병 조회</Link>
-        </li>
-        <li>
-          <Link to="/covid">코로나 조회</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <S.StyledBurger open={open} onClick={() => setOpen(!open)}>
+        <S.Menus open={open} />
+        <S.Menus open={open} />
+        <S.Menus open={open} />
+      </S.StyledBurger>
+      <S.Nav>
+        <NavLink to="/">
+          <S.Logo src={require("../assets/logo.png")} alt="Gustavo Scarpim" />
+        </NavLink>
+      </S.Nav>
+      <S.Ul open={open}>
+        <S.LogoUl src={require("../assets/logo.png")} alt={"Gustavo Scarpim"} />
+
+        <NavLink
+          to="/covid"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          <S.Li>코로나19</S.Li>
+        </NavLink>
+
+        <NavLink
+          to="/disease"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          <S.Li>증상 검색</S.Li>
+        </NavLink>
+
+        <NavLink
+          to="/drug"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          <S.Li>의약품 검색</S.Li>
+        </NavLink>
+
+        <NavLink
+          to="/hospital"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          <S.Li>의료기관 검색</S.Li>
+        </NavLink>
+      </S.Ul>
+    </>
   );
-}
+};
+
+export default NavBar;
