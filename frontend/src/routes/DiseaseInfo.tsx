0a68fe015505319c6ae0b8ea123453fadf438423
snowwhitewolf <whdgus1029@naver.com>
2022-03-20 12:17:02 +0900

    feat : 페이지 이동 구현

--- frontend/src/routes/DiseaseInfo.tsx
+++ frontend/src/routes/DiseaseInfo.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 export default function DiseaseInfo() {
   return (
     <div>
+      <NavBar />
       <h1>증상/질병 조회</h1>
     </div>
   );
--- frontend/src/routes/HospitalInfo.tsx
+++ frontend/src/routes/HospitalInfo.tsx
@@ -1,6 +1,16 @@
+import { useHistory } from "react-router-dom";
+
 export default function HospotalInfo() {
+  const history = useHistory();
+
+  function routeHome() {
+    history.push("/");
+  }
   return (
     <div>
+      <div onClick={routeHome}>
+        <p>홈</p>
+      </div>
       <h1>의료기관 조회</h1>
     </div>
   );
