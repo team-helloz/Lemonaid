c0456bcfa94597a172c6ba74c2b925de1a9feb8b
ek_13 <kldoggod@gmail.com>
2022-03-22 16:48:44 +0900

    feat : 부위 별 증상 선택 기능 구현

--- frontend/src/routes/DiseaseInfo.tsx
+++ frontend/src/routes/DiseaseInfo.tsx
@@ -2,54 +2,45 @@
 import NavBar from "../components/NavBar";
 import BodyPart from "../components/DiseaseInfo/BodyPart";
 import Symptom from "../components/DiseaseInfo/Symptom";
+import SelectedSymptom from "../components/DiseaseInfo/SelectedSymptom";
 import DiseaseList from "../components/DiseaseInfo/DiseaseList";
-// Type
-import { iconName } from "../components/DiseaseInfo/constants";
+// React
+import { useState } from "react";
 
 export default function DiseaseInfo() {
 
-  type Part = {
-    id: number,
-    eng: iconName,
-    kor: string,
+  const [nowPart, setPart] = useState<number>(0);
+  const [nowSymptoms, setSymptoms] = useState<string[]>([]);
+
+  function updatePart(nowPart: number): void {
+    setPart(nowPart);
   }
 
-  const partOne: Part[] = [
-    { id: 1, eng: "head", kor: "머리" },
-    { id: 2, eng: "neck", kor: "목" },
-    { id: 3, eng: "cest", kor: "가슴" },
-    { id: 4, eng: "stomach", kor: "배" },
-    { id: 5, eng: "back", kor: "등" },
-    { id: 6, eng: "buttocks", kor: "엉덩이" },
-    { id: 7, eng: "arm", kor: "팔" },
-    { id: 8, eng: "leg", kor: "다리" },
-    { id: 9, eng: "eye", kor: "눈" },
-    { id: 10, eng: "ear", kor: "귀" },
-  ]
+  function addSymptom(symptomname: string): void {
+    setSymptoms([...nowSymptoms, symptomname]);
+  }
 
-  const partTwo: Part[] = [
-    { id: 1, eng: "nose", kor: "코" },
-    { id: 1, eng: "mouth", kor: "입" },
-    { id: 1, eng: "body", kor: "전신" },
-    { id: 1, eng: "skin", kor: "피부" },
-    { id: 1, eng: "breast", kor: "유방" },
-    { id: 1, eng: "genitals", kor: "생식기" },
-    { id: 1, eng: "pelvis", kor: "골반" },
-    { id: 1, eng: "hand", kor: "손" },
-    { id: 1, eng: "foot", kor: "발" },
-    { id: 1, eng: "etc", kor: "기타" },
-  ]
+  function removeSymptom(nowSymptoms: string[]): void {
+    setSymptoms(nowSymptoms);
+  }
 
   return (
     <div>
       <NavBar />
       <h1>증상/질병 조회</h1>
-      <BodyPart parts={partOne}/>
+      <BodyPart
+        nowPart={nowPart}
+        updatePart={updatePart}
+      />
       <br />
-      <BodyPart parts={partTwo}/>
-      <br />
-      <p>증상 : </p>
-      <Symptom />
+      <SelectedSymptom
+        symptoms={nowSymptoms}
+        removeSymptom={removeSymptom}
+      />
+      <Symptom
+        nowPart={nowPart}
+        addSymptom={addSymptom}
+      />
       <br />
       <hr />
       <DiseaseList />
