0ef2b5eaa39695ac9e81c3be630a64bbe9387ac2
snowwhitewolf <whdgus1029@naver.com>
2022-03-23 13:06:56 +0900

    fix : medical 라우트 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./routes/Home";
-import HospotalInfo from "./routes/HospitalInfo";
+import MedicalInfo from "./routes/MedicalInfo";
 import DrugInfo from "./routes/DrugInfo";
-import DrugList from "./components/drug/DrugList";
-import DrugDetail from "./components/drug/DrugDetail";
 import DiseaseInfo from "./routes/DiseaseInfo";
 import DiseaseDetail from "./components/DiseaseInfo/DiseaseDetail";
 import CovidInfo from "./routes/CovidInfo";
@@ -15,10 +13,8 @@ function App() {
     <Router>
       {/* exact 경로 중복 방지 */}
       <Route path="/" exact component={Home} />
-      <Route path="/hospital" component={HospotalInfo} />
-      <Route path="/drug" component={DrugInfo} exact />
-      <Route path="/drug/list" component={DrugList} exact />
-      <Route path="/drug/list/:drugname" component={DrugDetail} />
+      <Route path="/medical" component={MedicalInfo} />
+      <Route path="/drug" component={DrugInfo} />
       <Route path="/disease" component={DiseaseInfo} exact />
       <Route path="/disease/:diseasename" component={DiseaseDetail} />
       <Route path="/covid" component={CovidInfo} />
