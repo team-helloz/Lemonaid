8a6050b8c63450fab4839978b089df3f57159954
snowwhitewolf <whdgus1029@naver.com>
2022-03-25 16:42:14 +0900

    feat : navbar css 수정

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,70 +1,59 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import * as S from "./styles";
+import { NavLink, useHistory } from "react-router-dom";
+import "./NavBar.css";
 
-const NavBar = () => {
-  const [open, setOpen] = useState(false);
-
+export default function NavBar() {
+  const history = useHistory();
+  function routeHome() {
+    history.push("/");
+  }
   return (
-    <S.NavSide>
-      <S.NavGroup>
-        <S.StyledBurger open={open} onClick={() => setOpen(!open)}>
-          <S.Menus open={open} />
-          <S.Menus open={open} />
-          <S.Menus open={open} />
-        </S.StyledBurger>
-        <S.Nav>
-          <NavLink to="/">
-            <S.Logo src={require("../assets/logo.png")} alt="Gustavo Scarpim" />
-          </NavLink>
-        </S.Nav>
-        <S.Ul open={open}>
-          <S.LogoUl src={require("../assets/logo.png")} alt={"Gustavo Scarpim"} />
-
-          <NavLink
-            to="/medical"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "#0DADEA",
-            }}
-          >
-            <S.Li>의료기관 검색</S.Li>
-          </NavLink>
-
-          <NavLink
-            to="/medicine"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "#0DADEA",
-            }}
-          >
-            <S.Li>의약품 검색</S.Li>
-          </NavLink>
-
-          <NavLink
-            to="/disease"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "#0DADEA",
-            }}
-          >
-            <S.Li>증상 검색</S.Li>
-          </NavLink>
-
-          <NavLink
-            to="/covid"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "#0DADEA",
-            }}
-          >
-            <S.Li>코로나19</S.Li>
-          </NavLink>
-          
-        </S.Ul>
-      </S.NavGroup>
-    </S.NavSide>
+    <div className="nav-container">
+      <div className="nav-content">
+        <img
+          className="nav-home"
+          src="/navHome.png"
+          alt=""
+          onClick={routeHome}
+        />
+        <NavLink
+          to="/medical"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          의료기관 찾기
+        </NavLink>
+        <NavLink
+          to="/medicine"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          의약품 조회
+        </NavLink>
+        <NavLink
+          to="/disease"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          질병 찾기
+        </NavLink>
+        <NavLink
+          to="/covid"
+          activeStyle={{
+            fontWeight: "bold",
+            color: "#0DADEA",
+          }}
+        >
+          코로나19
+        </NavLink>
+      </div>
+      <div className="nav-bottom"></div>
+    </div>
   );
-};
-
-export default NavBar;
+}
