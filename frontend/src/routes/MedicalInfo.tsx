d8f6d061241c616ad42a23f461be845b6312f97f
snowwhitewolf <whdgus1029@naver.com>
2022-03-25 13:35:59 +0900

    feat : 맵 컴포넌트 통합

--- frontend/src/routes/MedicalInfo.tsx
+++ frontend/src/routes/MedicalInfo.tsx
@@ -1,8 +1,17 @@
 import { useHistory } from "react-router-dom";
 import "./MedicalInfo.css";
-import MedicalMap from "../components/MedicalInfo/MedicalMap";
 import MedicalList from "../components/MedicalInfo/MedicalList";
-
+import { useEffect } from "react";
+import * as React from "react";
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+const nowCoor = {
+  y: 36.1072226,
+  x: 128.412915,
+};
 export default function MedicalInfo() {
   const history = useHistory();
 
@@ -68,6 +77,40 @@ export default function MedicalInfo() {
   function routeHome() {
     history.push("/");
   }
+  const [nowCenter, setNowCenter] = React.useState(nowCoor);
+
+  const userGeo = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude,
+          lon = position.coords.longitude;
+        setNowCenter({ y: lat, x: lon });
+      });
+    } else {
+    }
+    console.log(nowCenter);
+  };
+
+  const mapContainer = () => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(nowCenter.y, nowCenter.x),
+      level: 4,
+    };
+    console.log(options);
+    let map = new window.kakao.maps.Map(container, options);
+
+    var control = new window.kakao.maps.ZoomControl();
+    map.addControl(control, window.kakao.maps.ControlPosition.TOPRIGHT);
+  };
+  useEffect(() => {
+    userGeo();
+    mapContainer();
+  }, []);
+
+  useEffect(() => {
+    mapContainer();
+  }, [nowCenter]);
   return (
     <>
       <div className="medi-left">
@@ -83,7 +126,7 @@ export default function MedicalInfo() {
       <div className="medi-right">
         <MedicalList hospitals={hospitalList} />
       </div>
-      <MedicalMap />
+      <div id="map"></div>
     </>
   );
 }
