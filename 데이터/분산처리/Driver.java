d0172f3cc75352cc4cb3344dcf39b7e04992682d
이진곤 <qazqww@naver.com>
2022-03-25 11:16:39 +0900

    test : 하둡 테스트 코드 추가

---
+++ 데이터/분산처리/Driver.java
@@ -0,0 +1,22 @@
+package com.helloz.lemonaid;
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class Driver {
+    public static void main(String[] args) {
+        int exitCode = -1;
+        ProgramDriver pgd = new ProgramDriver();
+        try {
+            pgd.addClass("wordcount", Wordcount.class, "A map/reduce program");
+            pgd.addClass("medicalsubjectsearch", MedicalSubjectSearch.class, "");
+
+            pgd.driver(args);
+            exitCode = 0;
+        }
+        catch(Throwable e) {
+            e.printStackTrace();
+        }
+
+        System.exit(exitCode);
+    }
+}
---
+++ 데이터/분산처리/Wordcount.java
@@ -0,0 +1,96 @@
+package com.helloz.lemonaid;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Wordcount {
+	/* 
+	Object, Text : input key-value pair type (always same (to get a line of input file))
+	Text, IntWritable : output key-value pair type
+	*/
+	public static class TokenizerMapper
+			extends Mapper<Object,Text,Text,IntWritable> {
+
+		// variable declairations
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		// map function (Context -> fixed parameter)
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			// value.toString() : get a line
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while ( itr.hasMoreTokens() ) {
+				word.set(itr.nextToken());
+
+				// emit a key-value pair
+				context.write(word,one);
+			}
+		}
+	}
+
+	/*
+	Text, IntWritable : input key type and the value type of input value list
+	Text, IntWritable : output key-value pair type
+	*/
+	public static class IntSumReducer
+			extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+		// variables
+		private IntWritable result = new IntWritable();
+
+		// key : a disticnt word
+		// values :  Iterable type (data list)
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+				throws IOException, InterruptedException {
+
+			int sum = 0;
+			for ( IntWritable val : values ) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key,result);
+		}
+	}
+
+
+	/* Main function */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		if ( otherArgs.length != 2 ) {
+			System.err.println("Usage: <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf,"word count");
+		job.setJarByClass(Wordcount.class);
+
+		// let hadoop know my map and reduce classes
+		job.setMapperClass(TokenizerMapper.class);
+		job.setReducerClass(IntSumReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// set number of reduces
+		job.setNumReduceTasks(2);
+
+		// set input and output directories
+		FileInputFormat.addInputPath(job,new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1 );
+	}
+}
+
