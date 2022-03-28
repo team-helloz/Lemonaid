8722ddc3a2bfebd0c06e142d73308010f768b79a
이진곤 <qazqww@naver.com>
2022-03-28 18:19:36 +0900

    feat : 의약품 API 추가

---
+++ backend/src/main/java/com/helloz/lemonaid/request/MedicineSearchFilter.java
@@ -0,0 +1,36 @@
+package com.helloz.lemonaid.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("MedicineSearchFilter")
+public class MedicineSearchFilter {
+
+    @ApiModelProperty(name = "약품명", example = "레프로진정")
+    private String name;
+
+    @ApiModelProperty(name = "업체명", example = "위더스제약")
+    private String company;
+
+    @ApiModelProperty(name = "약품 모양", example = "원형")
+    private String shape;
+
+    @ApiModelProperty(name = "약품 색상", example = "하양")
+    private String color;
+
+    @ApiModelProperty(name = "약품 제형", example = "정제")
+    private String form;
+
+    @ApiModelProperty(name = "약품 분할선", example = "없음")
+    private String line;
+
+    @ApiModelProperty(name = "약품 마크", example = "없음")
+    private String mark;
+
+    @ApiModelProperty(name = "약품 식별문자", example = "WU / LV")
+    private String sign;
+}
---
+++ backend/src/main/java/com/helloz/lemonaid/service/MedicineServiceImpl.java
@@ -0,0 +1,29 @@
+package com.helloz.lemonaid.service;
+
+import com.helloz.lemonaid.db.entity.Medicine;
+import com.helloz.lemonaid.db.repository.MedicineRepository;
+import com.helloz.lemonaid.request.MedicineSearchFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MedicineServiceImpl implements MedicineService {
+
+    private final MedicineRepository medicineRepository;
+
+    @Override
+    public List<Medicine> getMedicineList(MedicineSearchFilter filter) {
+        List<Medicine> result = medicineRepository.searchByFilter(filter);
+        return result;
+    }
+
+    @Override
+    public Medicine getMedicine(long no) {
+        return medicineRepository.findByNo(no);
+    }
+}
