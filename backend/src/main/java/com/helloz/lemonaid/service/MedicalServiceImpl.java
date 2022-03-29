0b090b18e2e0dae14cde305a22bef5f35e6bab81
지수연 <zzzxxzxz@naver.com>
2022-03-29 16:48:47 +0900

    feat : 의료기관 조회 api 진료과목 추가

--- backend/src/main/java/com/helloz/lemonaid/service/MedicalServiceImpl.java
+++ backend/src/main/java/com/helloz/lemonaid/service/MedicalServiceImpl.java
@@ -1,15 +1,12 @@
 package com.helloz.lemonaid.service;
 
 import com.helloz.lemonaid.common.exception.NotFoundException;
-import com.helloz.lemonaid.db.entity.Hospital;
-import com.helloz.lemonaid.db.entity.Medical;
-import com.helloz.lemonaid.db.entity.MedicalSubject;
-import com.helloz.lemonaid.db.entity.Pharmacy;
+import com.helloz.lemonaid.db.entity.*;
+import com.helloz.lemonaid.db.repository.HospitalMedicalSubjectRepository;
 import com.helloz.lemonaid.db.repository.HospitalRepository;
 import com.helloz.lemonaid.db.repository.MedicalSubjectRepository;
 import com.helloz.lemonaid.db.repository.PharmacyRepository;
 import com.helloz.lemonaid.request.MedicalSearchFilter;
-import com.helloz.lemonaid.db.entity.MedicalType;
 import com.helloz.lemonaid.response.MedicalCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -27,28 +25,21 @@ public class MedicalServiceImpl implements MedicalService {
     private final HospitalRepository hospitalRepository;
     private final PharmacyRepository pharmacyRepository;
     private final MedicalSubjectRepository medicalSubjectRepository;
+    private final HospitalMedicalSubjectRepository hospitalMedicalSubjectRepository;
 
     @Override
     public List<Medical> getMedicalList(MedicalSearchFilter filter) {
         List<Medical> result = new ArrayList<>();
 
-        if (filter.getSearchType() == MedicalType.all) {
-            List<Hospital> hospitals = hospitalRepository.searchByFilter(filter);
-            List<Pharmacy> pharmacies = pharmacyRepository.searchByFilter(filter);
-
-            if (hospitals != null) result.addAll(hospitals);
-            if (pharmacies != null) result.addAll(pharmacies);
-
-        } else if (filter.getSearchType() == MedicalType.hospital) {
-            List<Hospital> hospitals = hospitalRepository.searchByFilter(filter);
-            if (hospitals != null) result.addAll(hospitals);
-
-        } else if (filter.getSearchType() == MedicalType.pharmacy) {
-            List<Pharmacy> pharmacies = pharmacyRepository.searchByFilter(filter);
-            if (pharmacies != null) result.addAll(pharmacies);
+        if (filter.getSearchType() != MedicalType.hospital) {
+            result.addAll(getHospitalList(filter));
+        }
+        if (filter.getSearchType() != MedicalType.pharmacy) {
+            result.addAll(getPharmacyList(filter));
         }
 
         Collections.sort(result);
+        log.info("호출, 크기: {}", result.size());
         return result;
     }
 
@@ -70,5 +61,19 @@ public class MedicalServiceImpl implements MedicalService {
     @Override
     public List<MedicalCode> getMedicalCodeList() {
         return hospitalRepository.findCodeAll();
+    }
+
+    private List<Hospital> getHospitalList(MedicalSearchFilter filter) {
+        List<Hospital> result = hospitalRepository.searchByFilter(filter);
+        result.forEach(
+                hospital -> hospital.setMedicalSubjectList(hospitalMedicalSubjectRepository.findAllByHospital(hospital)
+                        .stream().map(HospitalMedicalSubject::getMedicalSubject).collect(Collectors.toList()))
+        );
+
+        return result;
+    }
+
+    private List<Pharmacy> getPharmacyList(MedicalSearchFilter filter) {
+        return pharmacyRepository.searchByFilter(filter);
     }
 }
