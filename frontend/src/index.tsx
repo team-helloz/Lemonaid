94271bcb6006988f01fa81987fd8f813856c8886
sanghyun-lee2 <h983019@naver.com>
2022-03-29 18:03:41 +0900

    feat : 의료기관 조회 api 사용 기능 추가
    [S06P22D108-166]

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://j6d108.p.ssafy.io/api/v1";
 
 ReactDOM.render(
   <React.StrictMode>
