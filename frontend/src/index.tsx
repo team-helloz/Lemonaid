a58a04d03e2d67c113aa1c9520682a209ebbc558
snowwhitewolf <whdgus1029@naver.com>
2022-03-30 15:47:34 +0900

    feat : Redux 추가 및 증상선택 저장

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 axios.defaults.baseURL = "https://j6d108.p.ssafy.io/api/v1";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
