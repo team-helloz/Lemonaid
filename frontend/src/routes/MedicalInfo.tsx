cf79f3739a93611a21e96baf724b752f5aee4482
sanghyun-lee2 <h983019@naver.com>
2022-03-30 17:33:00 +0900

    feat : 의료기관 길찾기 기능 추가
    [S06P22D108-167]

--- frontend/src/routes/MedicalInfo.tsx
+++ frontend/src/routes/MedicalInfo.tsx
@@ -1,10 +1,10 @@
 import { useHistory } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MedicalList from "../components/MedicalInfo/MedicalList";
 import MedicalMap from "../components/MedicalInfo/MedicalMap";
 import MedicalDetail from "../components/MedicalInfo/MedicalDetail";
 import MedicalSubject from "../components/MedicalInfo/MedicalSubject";
-import { IHospital } from "../interface";
+import { IHospital, ICoord } from "../interface";
 import axios from "axios";
 import "./MedicalInfo.css";
 
@@ -15,27 +15,35 @@ import SymbolEmergency from "../assets/medical_png/symbol_emergency.png";
 import MenuOpen from "../assets/medical_png/menu_open.png";
 import MenuClose from "../assets/medical_png/menu_close.png";
 
-// 삼성 구미 2사업장
-const nowCoor = {
-  lat: 36.1072226,
-  lng: 128.412915,
-};
-
 export default function MedicalInfo() {
   const history = useHistory();
 
   function routeHome() {
     history.push("/");
   }
-  const [nowCenter, setNowCenter] = useState(nowCoor);
+  const [nowCenter, setNowCenter] = useState<ICoord>({
+    lat: 36.1072226,
+    lng: 128.412915,
+  });
+  const [viewCenter, setViewCenter] = useState<ICoord>({
+    lat: 36.1072226,
+    lng: 128.412915,
+  });
+  const [destCoord, setDestCoord] = useState<ICoord>({
+    lat: 36.1072226,
+    lng: 128.412915,
+  });
   const [roadAdd, setRoadAdd] = useState("");
   const [openDetailDlg, setOpenDetailDlg] = useState(false);
   const [openSubject, setOpenSubject] = useState(false);
   const [hospitalObj, setHospitalObj] = useState<IHospital>();
-  const [viewHospital, setViewHospital] = useState(false);
-  const [viewPharmacy, setViewPharmacy] = useState(false);
+  const [viewHospital, setViewHospital] = useState(true);
+  const [viewPharmacy, setViewPharmacy] = useState(true);
   const [subjects, setSubjects] = useState<number[]>([]);
   const [hospitalList, setHospitalList] = useState<IHospital[]>([]);
+  const [directionMode, setDirectionMode] = useState(false);
+
+  const isLoading = useRef(false);
 
   // 유저 위치 자동 추적
   const userGeo = () => {
@@ -44,17 +52,18 @@ export default function MedicalInfo() {
         var lat = position.coords.latitude,
           lng = position.coords.longitude;
         setNowCenter({ lat: lat, lng: lng });
-        xyToAdd(lng, lat);
+        setViewCenter({ lat: lat, lng: lng });
       });
-    } else {
     }
   };
+
   // 좌표로 주소 찾기
-  const xyToAdd = (lon: any, lat: any) => {
+  const xyToAdd = (lat: number, lng: number) => {
     const api = "8bfd93b5e35e8d6039d2b40188560f8b";
+    let addr = "";
     axios
       .get(
-        `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}`,
+        `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lng}&y=${lat}`,
         {
           headers: {
             Authorization: `KakaoAK ${api}`,
@@ -64,15 +73,17 @@ export default function MedicalInfo() {
       .then((res) => {
         // 좌표는 정확해서 조건문 생략
         // 주소 찾으면 현재 주소 바꾸기
-        if (res.data.documents[0].road_address !== null)
-          setRoadAdd(res.data.documents[0].road_address.address_name);
+        if (res.data.documents[0].address.address_name !== "") {
+          addr = res.data.documents[0].address.address_name;
+          setRoadAdd(addr);
+        }
+        isLoading.current = true;
       })
       .catch((e) => console.log(e));
   };
   // 주소로 좌표 찾기
   const addToXy = (address: any) => {
     const api = "8bfd93b5e35e8d6039d2b40188560f8b";
-    console.log(address);
     axios
       .get(
         `https://dapi.kakao.com/v2/local/search/address.json?query=${address}`,
@@ -88,7 +99,7 @@ export default function MedicalInfo() {
         // 결과값이 있으면
         if (lng !== null && lng !== undefined) {
           // 현재 위치 옮기기
-          setNowCenter({ lng, lat });
+          setNowCenter({ lat: lng, lng: lat });
           // 현재 주소 바꾸기
           setRoadAdd(res.data.documents[0].road_address.address_name);
         }
@@ -97,13 +108,26 @@ export default function MedicalInfo() {
   };
 
   const getMedicalList = () => {
+    if (isLoading.current === false) return;
+
+    let serachType = "all";
+    if (viewHospital === true && viewPharmacy === true) {
+      serachType = "all";
+    } else if (viewHospital === true) {
+      serachType = "hospital";
+    } else if (viewPharmacy === true) {
+      serachType = "pharmacy";
+    } else {
+      setHospitalList([]);
+      return;
+    }
     axios
       .get("/medical", {
         params: {
-          search_type: "all",
-          lat: nowCenter.lat,
-          lng: nowCenter.lng,
-          radius: 5000,
+          search_type: serachType,
+          lat: viewCenter.lat,
+          lng: viewCenter.lng,
+          radius: 1000,
           // subjects: subjects.join(","),
         },
       })
@@ -126,8 +150,13 @@ export default function MedicalInfo() {
   }, [subjects]);
 
   useEffect(() => {
+    xyToAdd(nowCenter.lat, nowCenter.lng);
     getMedicalList();
-  }, [viewHospital]);
+  }, [nowCenter]);
+
+  useEffect(() => {
+    getMedicalList();
+  }, [viewHospital, viewPharmacy]);
 
   // 병원 상세보기창 닫기
   const handDetailClose = () => {
@@ -140,15 +169,24 @@ export default function MedicalInfo() {
     setOpenDetailDlg(true);
   };
 
-  // 주소 검색 시 좌표변환 실행
-  const handleKeyPress = (e: any) => {
-    if (e.key === "Enter") {
-      addToXy(e.target.value);
-    }
-  };
-
   const UpdateSubjects = (subjectsData: number[]) => {
     setSubjects(subjectsData);
+  };
+
+  const SetDirectionMode = (destCoord: ICoord) => {
+    setDestCoord(destCoord);
+    setDirectionMode(true);
+    setOpenDetailDlg(false);
+  };
+
+  const handleViewAll = () => {
+    if (viewHospital === true && viewPharmacy === true) {
+      setViewHospital(false);
+      setViewPharmacy(false);
+    } else {
+      setViewHospital(true);
+      setViewPharmacy(true);
+    }
   };
 
   return (
@@ -164,7 +202,9 @@ export default function MedicalInfo() {
         <input type="text" placeholder={roadAdd} onKeyPress={handleKeyPress} />
       </div> */}
       <div className="medi-bottom">
-        <div className="medi-bottom-menu-first-item">전체 조회</div>
+        <div className="medi-bottom-menu-first-item" onClick={handleViewAll}>
+          전체 조회
+        </div>
         <div
           className="medi-bottom-menu-item"
           onClick={() => setViewHospital(!viewHospital)}
@@ -172,7 +212,10 @@ export default function MedicalInfo() {
           <img src={SymbolHospital} alt="" width={"17px"} />
           병원 조회
         </div>
-        <div className="medi-bottom-menu-item">
+        <div
+          className="medi-bottom-menu-item"
+          onClick={() => setViewPharmacy(!viewPharmacy)}
+        >
           <img src={SymbolPharmacy} alt="" width={"17px"} />
           약국 조회
         </div>
@@ -195,20 +238,25 @@ export default function MedicalInfo() {
       <div className="medi-right">
         <MedicalList
           hospitals={hospitalList}
-          setNowCenter={setNowCenter}
+          setViewCenter={setViewCenter}
           handDetailOpen={handDetailOpen}
         />
       </div>
       <MedicalMap
-        nowCenter={nowCenter}
-        setNowCenter={setNowCenter}
+        viewCenter={viewCenter}
+        setViewCenter={setViewCenter}
         hospitals={hospitalList}
         handDetailOpen={handDetailOpen}
+        directionMode={directionMode}
+        nowCenter={nowCenter}
+        destCoord={destCoord}
       ></MedicalMap>
       <MedicalDetail
         open={openDetailDlg}
         onClose={handDetailClose}
         hospitalDetail={hospitalObj}
+        roadAdd={roadAdd}
+        SetDirectionMode={SetDirectionMode}
       />
       <MedicalSubject
         openSubject={openSubject}
