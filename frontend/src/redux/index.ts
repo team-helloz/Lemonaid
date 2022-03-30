5f03d671c5901a984263ab506dcca0df2875637e
snowwhitewolf <whdgus1029@naver.com>
2022-03-30 16:05:55 +0900

    feat : 의약품 리덕스 스토어 및 리듀서 생성

--- frontend/src/redux/index.ts
+++ frontend/src/redux/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import diseaseReducer from "./Disease/reducer";
+import medicineReducer from "./Medicine/reducer";
 
 const rootReducer = combineReducers({
   diseaseReducer,
+  medicineReducer,
 });
 
 export default rootReducer;
