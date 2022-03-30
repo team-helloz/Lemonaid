9aaf9e0ae94f20d3bf6158e1396fcca661d3d254
ek_13 <kldoggod@gmail.com>
2022-03-30 17:07:30 +0900

    feat : Navbar 토픽 기능 추가

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,10 +2,35 @@ import { NavLink, useHistory } from "react-router-dom";
 import "./NavBar.css";
 
 export default function NavBar() {
+
   const history = useHistory();
   function routeHome() {
     history.push("/");
   }
+
+  const CardNews = [
+    {
+      title: "[코로나 19] 궁금해요! 소아 코로나 19 예방접종(심화편)",
+      url: "https://www.kdca.go.kr/gallery.es?mid=a20503010000&bid=0002&b_list=9&act=view&list_no=145621&nPage=1&vlist_no_npage=1&keyField=&keyWord=&orderby="
+    },
+    {
+      title: "[코로나 19] 궁금해요! 소아 코로나 19 예방접종(기초편)",
+      url: "https://www.kdca.go.kr/gallery.es?mid=a20503010000&bid=0002&b_list=9&act=view&list_no=145617&nPage=1&vlist_no_npage=1&keyField=&keyWord=&orderby="
+    },
+    {
+      title: "[코로나 19] 코로나 19 진단검사 체계개편 팩트체크",
+      url: "https://www.kdca.go.kr/gallery.es?mid=a20503010000&bid=0002&b_list=9&act=view&list_no=145540&nPage=3&vlist_no_npage=5&keyField=&keyWord=&orderby="
+    },
+    {
+      title: "2022년 보건복지부 정책 정리",
+      url: "https://blog.naver.com/mohw2016/222609396566"
+    },
+    {
+      title: "[코로나 19] 궁금해요! 소아 코로나 19 예방접종(심화편)",
+      url: "https://www.kdca.go.kr/gallery.es?mid=a20503010000&bid=0002&b_list=9&act=view&list_no=145621&nPage=1&vlist_no_npage=1&keyField=&keyWord=&orderby="
+    },
+  ]
+
   return (
     <div className="nav-container">
       <div className="nav-content">
@@ -52,7 +77,23 @@ export default function NavBar() {
           코로나
         </NavLink>
       </div>
-      <div className="nav-bottom"></div>
+      <div className="nav-bottom">
+        <div className="nav-bottom-content">
+          <p>HOT TOPIC : </p>
+          <div className="nav-news-group">
+            {CardNews.map((news, i: number) => (
+              <div className="nave-news-item">
+                <a
+                  key={i}
+                  href={news.url}
+                  target="_blank"
+                  className="nav-news-title"
+                >{news.title}</a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
