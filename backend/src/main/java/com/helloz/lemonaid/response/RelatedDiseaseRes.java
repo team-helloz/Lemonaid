a4410ef2138cef90394aa1afd559886488ad6dea
지수연 <zzzxxzxz@naver.com>
2022-04-03 01:46:28 +0900

    fix : 의료 목록 조회 페이지네이션 버그 수정 및 이름 변경
    - 병원 테이블, 약국 테이블 병합후 한 번에 처리하도록 수정

--- backend/src/main/java/com/helloz/lemonaid/response/RelatedDiseaseRes.java
+++ backend/src/main/java/com/helloz/lemonaid/response/RelatedDiseaseRes.java
@@ -3,19 +3,18 @@ package com.helloz.lemonaid.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.helloz.lemonaid.db.entity.Disease;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class RelatedDiseaseInfo {
+public class RelatedDiseaseRes {
     private long no;
     private String name;
 
-    public static RelatedDiseaseInfo of(Disease disease) {
-        RelatedDiseaseInfo res = new RelatedDiseaseInfo();
+    public static RelatedDiseaseRes of(Disease disease) {
+        RelatedDiseaseRes res = new RelatedDiseaseRes();
         res.setNo(disease.getNo());
         int index = disease.getName().indexOf('(');
         if (index != -1)
