ebe157f1d4561c3920784c64d196f896baaa97f4
지수연 <zzzxxzxz@naver.com>
2022-04-03 13:30:49 +0900

    fix :의료기관 조회 시 현재 위치와의 거리 순 정렬

--- backend/src/main/java/com/helloz/lemonaid/db/repository/PharmacyRepository.java
+++ backend/src/main/java/com/helloz/lemonaid/db/repository/PharmacyRepository.java
@@ -15,12 +15,13 @@ public interface PharmacyRepository extends JpaRepository<Pharmacy, Long>{
     @Query(
             value = "select distinct p.medical_no, p.opentime_no, p.medical_name, p.medical_tel," +
                     "p.medical_x, p.medical_y, p.medical_address, p.medical_parking_count, " +
-                    "ST_DISTANCE_SPHERE(ST_GEOMFROMTEXT(concat('POINT(', :#{#filter.mapLat}, ' ', :#{#filter.mapLng}, ')'), 4326), p.medical_point) AS distance" +
+                    " ST_DISTANCE_SPHERE(ST_GEOMFROMTEXT(concat('POINT(', :#{#filter.mapLat}, ' ', :#{#filter.mapLng}, ')'), 4326), p.medical_point) AS mapDistance" +
+                    " ST_DISTANCE_SPHERE(ST_GEOMFROMTEXT(concat('POINT(', :#{#filter.nowLat}, ' ', :#{#filter.nowLng}, ')'), 4326), p.medical_point) AS nowDistance" +
                     " from medical p" +
                     " where p.medical_type ='P'" +
                     " and (:#{#filter.parking} is false or p.medical_parking_count > 0)" +
                     " and (:#{#filter.keyword} = '' or p.medical_name like concat('%',:#{#filter.keyword},'%'))" +
-                    " having distance <= :#{#filter.radius} order by distance"
+                    " having mapDistance <= :#{#filter.radius} order by nowDistance"
 
     ,nativeQuery = true)
     List<Pharmacy> searchByFilter(@Param("filter") MedicalSearchFilter filter, Pageable pageable);
