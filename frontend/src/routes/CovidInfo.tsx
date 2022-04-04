c0ba91094328618d36bbcf1a2c42204ad61dc7bf
p9a3r5k0 <p9a3r5k0@naver.com>
2022-04-04 21:59:54 +0900

    feat : 코로나 성별별 차트

--- frontend/src/routes/CovidInfo.tsx
+++ frontend/src/routes/CovidInfo.tsx
@@ -1,13 +1,18 @@
 import NavBar from "../components/NavBar";
 import "./CovidInfo.css";
 import Footer from "../components/Footer";
+
+import { CovidGenderChart } from "../components/Covid/CovidGenderChart";
+
 export default function CovidInfo() {
   return (
     <div>
       <NavBar />
       <div className="covid-container">
         <div className="covid-2container">
-          <div className="today-covid"></div>
+          <div className="today-covid">
+            <CovidGenderChart />
+          </div>
           <div className="today-covid"></div>
         </div>
         <div className="covid-oneline"></div>
