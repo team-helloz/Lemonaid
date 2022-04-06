6ebbe49b33675c8fb21b47a215cc573dfdd27c77
snowwhitewolf <whdgus1029@naver.com>
2022-04-07 02:17:55 +0900

    feat : 코로나 그리드 수정

--- frontend/src/components/Covid/CovidCountChart.tsx
+++ frontend/src/components/Covid/CovidCountChart.tsx
@@ -1,92 +1,106 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import faker from '@faker-js/faker';
+import faker from "@faker-js/faker";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-        position: 'top' as const,
-        },
-        title: {
-        display: true,
-        text: '날짜 별 확진자 수',
-        },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
     },
+    title: {
+      display: true,
+      text: "날짜 별 확진자 수",
+    },
+  },
 };
 
 export function CovidCountChart() {
+  const [covidDateArr, setCovidDateArr] = useState([]);
+  const [covidCountArr, setCovidCountArr] = useState([]);
 
-    const [covidDateArr, setCovidDateArr] = useState([]);
-    const [covidCountArr, setCovidCountArr] = useState([]);
+  const [covidChartDate, setCovidChartDate] = useState([]);
+  const [covidChartCount, setCovidChartCount] = useState([]);
 
-    const [covidChartDate, setCovidChartDate] = useState([]);
-    const [covidChartCount, setCovidChartCount] = useState([]);
+  const [flagBtn, setFlagBtn] = useState(31);
 
-    const [flagBtn, setFlagBtn] = useState(31);
+  const handleLoad = () => {
+    axios
+      .get("/corona/count")
+      .then((res) => {
+        setCovidDateArr(
+          res.data.slice(0, 31).map((row: any) => row.count_date.split("T")[0])
+        );
+        setCovidCountArr(
+          res.data.slice(0, 31).map((row: any) => row.count_total)
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const handleLoad = () => {
-        axios
-          .get('/corona/count')
-          .then((res) => {
-              setCovidDateArr(res.data.slice(0, 31).map((row: any) => row.count_date.split("T")[0]))
-              setCovidCountArr(res.data.slice(0, 31).map((row: any) => row.count_total))
-          })
-          .catch((err) => {
-              console.log(err)
-          })
-    }
+  useEffect(() => {
+    handleLoad();
+  }, []);
 
-    useEffect(() => {
-        handleLoad();
-    }, []);
+  const sevenBtn = () => {
+    setFlagBtn(8);
+  };
 
-    const sevenBtn = () => {
-        setFlagBtn(8);
-    }
+  const thirtyBtn = () => {
+    setFlagBtn(31);
+  };
 
-    const thirtyBtn = () => {
-        setFlagBtn(31);
-    }
+  const labels = covidDateArr.slice(0, flagBtn).reverse();
 
-    const labels = covidDateArr.slice(0, flagBtn).reverse();
-
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: '국내 총 확진자 수',
-                data: covidCountArr.slice(0, flagBtn).reverse(),
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            },
-        ],
-    };
-    return (
-        <div>
-            <button onClick={sevenBtn}>7일</button>
-            <button onClick={thirtyBtn}>30일</button>
-            <Bar options={options} data={data} />
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "국내 총 확진자 수",
+        data: covidCountArr.slice(0, flagBtn).reverse(),
+        backgroundColor: "rgb(6, 190, 182)",
+      },
+    ],
+  };
+  return (
+    <div>
+      <div className="covid-btn-container">
+        <div
+          className={flagBtn === 8 ? "thdays-btn" : "sedays-btn"}
+          onClick={sevenBtn}
+        >
+          7일
         </div>
-    );
+        <div
+          className={flagBtn === 31 ? "thdays-btn" : "sedays-btn"}
+          onClick={thirtyBtn}
+        >
+          30일
+        </div>
+      </div>
+      <Bar options={options} data={data} style={{ margin: "20px" }} />
+    </div>
+  );
 }
-  
--- frontend/src/components/Covid/CovidGenderChart.tsx
+++ frontend/src/components/Covid/CovidGenderChart.tsx
@@ -1,83 +1,72 @@
 import { useEffect, useState } from "react";
 
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 import axios from "axios";
+import { width } from "@mui/system";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
 interface CovidGenderType {
-    num: number;
-    date: Date;
-    gender_men: number;
-    gender_women: number;
-    gender_total_men: number;
-    gender_total_women: number;
+  num: number;
+  date: Date;
+  gender_men: number;
+  gender_women: number;
+  gender_total_men: number;
+  gender_total_women: number;
 }
 
-
 export function CovidGenderChart() {
+  const [covidMenTotal, setCovidMenTotal] = useState(0);
+  const [covidWomenTotal, setCovidWomenTotal] = useState(0);
 
-    const [covidMenTotal, setCovidMenTotal] = useState(0);
-    const [covidWomenTotal, setCovidWomenTotal] = useState(0);
-
-    const options: any = {
-        plugins: {
-            title: {
-                display: true,
-                text: '성별별 코로나 확진자 수'
-            },
-            legend: {
-                display: true,
-            },
+  const options: any = {
+    plugins: {
+      title: {
+        display: true,
+        text: "성별별 코로나 확진자 수",
+      },
+      legend: {
+        display: true,
+      },
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem: any) {
+          return tooltipItem.yLabel;
         },
-        tooltips: {
-            callbacks: {
-               label: function(tooltipItem : any) {
-                      return tooltipItem.yLabel;
-               }
-            }
-        }
-    }
+      },
+    },
+  };
 
-    const handleLoad = () => {
-        axios
-          .get('/corona/gender')
-          .then((res) => {
-            //   console.log(res.data);
-            setCovidMenTotal(res.data.corona_men_total);
-            setCovidWomenTotal(res.data.corona_women_total);
-          })
-          .catch((err) => {
-              console.log(err);
-          })
-    }
+  const handleLoad = () => {
+    axios
+      .get("/corona/gender")
+      .then((res) => {
+        setCovidMenTotal(res.data.corona_men_total);
+        setCovidWomenTotal(res.data.corona_women_total);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    useEffect(() => {
-        handleLoad();
-    }, []);
+  useEffect(() => {
+    handleLoad();
+  }, []);
 
-    const data = {
-        labels: ['여성 확진자', '남성 확진자'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [covidWomenTotal, covidMenTotal],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.8)',
-              'rgba(54, 162, 235, 0.8)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-    }
+  const data = {
+    labels: ["여성 확진자", "남성 확진자"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [covidWomenTotal, covidMenTotal],
+        backgroundColor: ["rgba(255, 99, 132, 0.8)", "rgba(54, 162, 235, 0.8)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    return (
-        <Pie data={data} options={options} />
-    )
+  return <Pie data={data} options={options} style={{ margin: "20px" }} />;
 }
