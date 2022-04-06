7dda9dfacc456d0a4f6db3c679ba88afec9d2f89
snowwhitewolf <whdgus1029@naver.com>
2022-04-06 15:04:30 +0900

    feat : 병원 종류 리스트 수정

--- frontend/src/components/MedicalInfo/MedicalCodeItem.tsx
+++ frontend/src/components/MedicalInfo/MedicalCodeItem.tsx
@@ -14,15 +14,7 @@ export default function MedicalCodeItem(props: MedicalCodeItemProps) {
         "medical-code-item" + (itemName === selectedCodeName ? "-selected" : "")
       }
     >
-      <div className="medical-code-item-img">
-        <img
-          className="medicine-code-img"
-          src={`/MedicineSearch/기타.png`}
-          alt=""
-          width={40}
-          height={40}
-        />
-      </div>
+      <div className="medical-code-item-img"></div>
       <p className="medical-code-item-text">{itemName}</p>
     </div>
   );
