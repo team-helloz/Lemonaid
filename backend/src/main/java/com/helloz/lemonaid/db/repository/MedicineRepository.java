cd5cf64e685328bd863fa7077803970dfec3c2a5
이진곤 <qazqww@naver.com>
2022-04-06 15:29:56 +0900

    fix : 의약품 검색 식별문자에서 print column 추가

--- backend/src/main/java/com/helloz/lemonaid/db/repository/MedicineRepository.java
+++ backend/src/main/java/com/helloz/lemonaid/db/repository/MedicineRepository.java
@@ -22,7 +22,7 @@ public interface MedicineRepository extends JpaRepository<Medicine, Long> {
             " and (:color = '전체' or (medicine_color_front like concat('%', :color, '%') or medicine_color_back like concat('%', :color, '%')))" +
             " and (:form = -1 or medicine_form_code_no = :form)" +
             " and (:line = '전체' or (medicine_line_front = :line or medicine_line_back = :line))" +
-            " and (:sign = '전체' or (medicine_mark_anal_front like concat('%', :sign, '%') or medicine_mark_anal_front like concat('%', :sign, '%')))" +
+            " and (:sign = '전체' or (medicine_mark_anal_front like concat('%', :sign, '%') or medicine_mark_anal_front like concat('%', :sign, '%') or medicine_print_front like concat('%', :sign, '%') or medicine_print_back like concat('%', :sign, '%')))" +
             " and (:name is null or medicine_name like concat('%', :name, '%'))")
     List<Medicine> searchByFilter(@Param("name") String name, @Param("shape") String shape, @Param("color") String color,
                                   @Param("form") int form, @Param("line") String line, @Param("sign") String sign, Pageable pageable);
@@ -32,7 +32,7 @@ public interface MedicineRepository extends JpaRepository<Medicine, Long> {
             " and (:color = '전체' or (medicine_color_front like concat('%', :color, '%') or medicine_color_back like concat('%', :color, '%')))" +
             " and (:form = -1 or medicine_form_code_no = :form)" +
             " and (:line = '전체' or (medicine_line_front = :line or medicine_line_back = :line))" +
-            " and (:sign = '전체' or (medicine_mark_anal_front like concat('%', :sign, '%') or medicine_mark_anal_front like concat('%', :sign, '%')))" +
+            " and (:sign = '전체' or (medicine_mark_anal_front like concat('%', :sign, '%') or medicine_mark_anal_front like concat('%', :sign, '%') or medicine_print_front like concat('%', :sign, '%') or medicine_print_back like concat('%', :sign, '%')))" +
             " and (:name is null or medicine_name like concat('%', :name, '%'))")
     int countByFilter(@Param("name") String name, @Param("shape") String shape, @Param("color") String color,
                       @Param("form") int form, @Param("line") String line, @Param("sign") String sign);
