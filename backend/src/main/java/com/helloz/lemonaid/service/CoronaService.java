118a73138d7d0b85c6e66ebef75767495aee2160
지수연 <zzzxxzxz@naver.com>
2022-04-06 11:57:12 +0900

    feat : 코로나 최다 발생 지역 api 구현

--- backend/src/main/java/com/helloz/lemonaid/service/CoronaService.java
+++ backend/src/main/java/com/helloz/lemonaid/service/CoronaService.java
@@ -3,6 +3,7 @@ package com.helloz.lemonaid.service;
 import com.helloz.lemonaid.db.entity.CoronaCount;
 import com.helloz.lemonaid.response.CoronaAgeRes;
 import com.helloz.lemonaid.response.CoronaCountTodayRes;
+import com.helloz.lemonaid.response.CoronaCountTop3Res;
 import com.helloz.lemonaid.response.CoronaGenderRes;
 
 import java.util.List;
@@ -16,4 +17,6 @@ public interface CoronaService {
     CoronaAgeRes getCoronaAgeList();
 
     CoronaCountTodayRes getCoronaCountToday();
+
+    CoronaCountTop3Res getCoronaCountTop3();
 }
